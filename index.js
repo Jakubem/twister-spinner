51dd1b371dff02cabd189922bf64a017aeb318f2
Jakub Maksimowicz <jakubemx@iMac-Functionite.local>
2017-07-13 23:15:22 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var colors = ['#5c5cff', '#ff5e57', '#6bc44b', '#f0f04e'];
+var limbs = ['left arm', 'right arm', 'left leg', 'right leg'];
+var output = document.querySelector('h1');
+
+function loop() {
+
+    var pick_colors = Math.floor(Math.random() * colors.length);
+    var pick_limbs = Math.floor(Math.random() * limbs.length);
+
+    var result_limbs = limbs[pick_limbs];
+    var result_colors = colors[pick_colors];
+
+    output.textContent = result_limbs;
+    output.style.background = result_colors;
+}
+
+var intervalTime = '5000';
+
+
+loop();
+setInterval(loop, intervalTime);
+
+
+
+var loading_bar = document.querySelector('.loading');
+
+loading_bar.style.animationDuration = intervalTime + 'ms';
+loading_bar.style.animationPlayState = 'running';
